214ed7f914aa92308e5e279ceb31c296db607e96
Ali Biner <alibiner.github@gmail.com>
2025-10-11 15:48:31 +0300

    initial web api

---
+++ webAPI/src/main/java/com/alibiner/veterinary_management_system/VeterinaryManagementSystemApplication.java
@@ -0,0 +1,19 @@
+package com.alibiner.veterinary_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.alibiner"})
+@EnableJpaRepositories("com.alibiner.repositories")
+@EntityScan("com.alibiner.entities")
+public class VeterinaryManagementSystemApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(VeterinaryManagementSystemApplication.class, args);
+    }
+
+}
