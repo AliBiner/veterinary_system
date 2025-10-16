ce1ce58b2f498a88411f278e5b8048ac0a64dc50
Ali Biner <alibiner.github@gmail.com>
2025-10-16 15:25:12 +0300

    feat: create User repository
    Signed-off-by: Ali Biner <alibiner.github@gmail.com>

---
+++ data-access/src/main/java/com/alibiner/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.alibiner.repositories;
+
+import java.util.*;
+import com.alibiner.entities.User;
+import com.alibiner.enums.UserType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface UserRepository extends JpaRepository<User, UUID>, JpaSpecificationExecutor<User> {
+
+    Optional<User> findByIdAndIsDeleteFalseAndUserType(UUID id, UserType userType);
+
+}
